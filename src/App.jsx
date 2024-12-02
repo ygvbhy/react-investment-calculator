2475fb576f215908ef67a408a31db88697eda583
ygvbhy <ygvbhy@naver.com>
2024-12-02 23:20:50 +0900

    end

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,8 @@ function App() {
     duration: 10,
   });
 
+  const inputIsValid = userInput.duration >= 1;
+
   function handleChange(inputIdentifier, newValue) {
     setUserInput((prevUserInput) => {
       return {
@@ -24,7 +26,10 @@ function App() {
     <>
       <Header />
       <UserInput onChange={handleChange} userInput={userInput} />
-      <Results input={userInput} />
+      {inputIsValid && <Results input={userInput} />}
+      {!inputIsValid && (
+        <p className="center">Please enter a duration greater than zero.</p>
+      )}
     </>
   );
 }
